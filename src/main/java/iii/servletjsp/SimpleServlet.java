5f5d589933641785dd8001765681a38256351513
User <User@EEIT-PC>
2019-05-02 19:16:21 +0800

    這是更新過的SimpleServlet

--- src/main/java/iii/servletjsp/SimpleServlet.java
+++ src/main/java/iii/servletjsp/SimpleServlet.java
@@ -13,7 +13,7 @@ public class SimpleServlet extends HttpServlet {
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		System.out.println("這是doGet()方法印出的訊息");
+		System.out.println("這是doGet()方法印出的訊息一");
 	}
 
 	@Override
